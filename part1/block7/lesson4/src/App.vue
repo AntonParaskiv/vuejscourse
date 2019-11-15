b2b833ff7cfeb95fabbbdd0616db35e95cbb73fc
Anton Paraskiv <anton@paraskiv.net>
2019-11-15 21:17:01 +0300

    Added part1/block7/lesson4

---
+++ part1/block7/lesson4/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2>Forms inputs</h2>
+
+    <label>
+      <input type="radio" value="instagram" v-model="social"> Instagram
+    </label>
+
+    <label>
+      <input type="radio" value="vk" v-model="social"> Vk
+    </label>
+
+    <label>
+      <input type="radio" value="facebook" v-model="social"> Facebook
+    </label>
+
+    <hr>
+    <p> {{ social }}</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            social: 'facebook',
+        }
+    },
+}
+</script>
+
+<style scoped>
+  textarea {
+    height: 100px;
+    width: 400px;
+  }
+
+  p {
+    white-space: pre;
+  }
+</style>
