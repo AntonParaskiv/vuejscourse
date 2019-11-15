b6848dd52aae12a324a71cf6dd31546288443408
Anton Paraskiv <anton@paraskiv.net>
2019-11-15 21:03:48 +0300

    Added part1/block7/lesson2

---
+++ part1/block7/lesson2/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Forms inputs</h2>
+    <textarea v-model="textarea"></textarea>
+    <p> {{ textarea }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            textarea: 'I am initial text',
+        }
+    },
+}
+</script>
+
+<style scoped>
+  textarea {
+    height: 100px;
+    width: 400px;
+  }
+
+  p {
+    white-space: pre;
+  }
+</style>
