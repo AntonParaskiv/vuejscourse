88328210c42db60b07bbe90fbade36611ef30e76
Anton Paraskiv <anton@paraskiv.net>
2019-11-15 20:57:23 +0300

    Added part1/block7/lesson1

---
+++ part1/block7/lesson1/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Forms inputs</h2>
+    <input type="text" v-model.lazy="name">
+
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name: 'Initial state',
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
