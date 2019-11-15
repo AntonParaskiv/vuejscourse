d63a767f66d1fb22754e1b8b9b56a57c2b5cea49
Anton Paraskiv <anton@paraskiv.net>
2019-11-15 21:38:33 +0300

    Added part1/block7/lesson6

---
+++ part1/block7/lesson6/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Forms inputs</h2>
+
+    <input type="text" v-model.number="age">
+    <p> {{ age }}</p>
+
+  </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                age: 20,
+            }
+        },
+        watch:{
+            age(value) {
+                console.log(value);
+                console.log(typeof value);
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
