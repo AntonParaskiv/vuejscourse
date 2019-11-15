40a9088058032595b36bee66654faeaaad9c762c
Anton Paraskiv <anton@paraskiv.net>
2019-11-15 21:24:40 +0300

    Added part1/block7/lesson5

---
+++ part1/block7/lesson5/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Forms inputs</h2>
+
+    <select v-model="social">
+      <option
+        v-for="s in socialsList"
+      >{{ s }}</option>
+    </select>
+
+    <hr>
+    <p>{{ social }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            defaultSocial: 'instagram',
+            social: 'instagram',
+            socialsList: ['instagram', 'vk', 'facebook'],
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
