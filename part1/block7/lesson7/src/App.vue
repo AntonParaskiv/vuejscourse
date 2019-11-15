1f72b2c5eb33fcde18a858f8e3debf91f5f63db9
Anton Paraskiv <anton@paraskiv.net>
2019-11-15 22:02:07 +0300

    Added part1/block7/lesson7

---
+++ part1/block7/lesson7/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Forms inputs</h2>
+
+    <app-onoff v-model="switched"></app-onoff>
+
+    <div>
+      <h3 v-if="switched">Component is enabled</h3>
+      <h3 v-else>Component is disabled</h3>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Onoff from "./Onoff";
+
+    export default {
+        data(){
+            return {
+                switched: true,
+            }
+        },
+        components: {
+            appOnoff: Onoff,
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
