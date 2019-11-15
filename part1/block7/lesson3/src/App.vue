e2a6d9b4f346447fe17c7436d64c2a4a42419b19
Anton Paraskiv <anton@paraskiv.net>
2019-11-15 21:11:41 +0300

    Added part1/block7/lesson3

---
+++ part1/block7/lesson3/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>Forms inputs</h2>
+
+    <label>
+      <input type="checkbox" value="instagram" v-model="social"> Instagram
+    </label>
+
+    <label>
+      <input type="checkbox" value="vk" v-model="social"> Vk
+    </label>
+
+    <label>
+      <input type="checkbox" value="facebook" v-model="social"> Facebook
+    </label>
+
+    <hr>
+    <ul>
+      <li v-for="s in social"> {{ s }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            social: [ 'vk' ],
+        }
+    },
+}
+</script>
+
+<style scoped>
+  textarea {
+    height: 100px;
+    width: 400px;
+  }
+
+  p {
+    white-space: pre;
+  }
+</style>
