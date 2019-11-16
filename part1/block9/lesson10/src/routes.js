3b42ca9e45563b2dfe596c78bad8d895dc42b17b
Anton Paraskiv <anton@paraskiv.net>
2019-11-16 15:28:41 +0300

    Added part1/block9/lesson10

---
+++ part1/block9/lesson10/src/routes.js
@@ -0,0 +1,58 @@
+import VueRouter from 'vue-router'
+import Home from './pages/Home'
+import Cars from './pages/Cars'
+import Car from './pages/Car'
+import ErrorCmp from './pages/Error'
+import CarFull from './pages/CarFull'
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '',
+      component: Home,
+    },
+    {
+      path: '/cars',
+      component: Cars,
+      name: 'cars',
+    },
+    {
+      path: '/car/:id',
+      component: Car,
+      children: [
+        {
+          path: 'full',
+          component: CarFull,
+          name: 'carFull',
+        },
+      ],
+    },
+    {
+      path: '/none',
+      redirect: {
+        name: 'cars',
+      },
+    },
+    {
+      path: '*',
+      component: ErrorCmp,
+    },
+  ],
+  mode: 'history',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+
+    if (to.hash) {
+      return {
+        selector: to.hash,
+      }
+    }
+
+    return {
+     x: 0,
+     y: 0,
+    }
+  },
+})
