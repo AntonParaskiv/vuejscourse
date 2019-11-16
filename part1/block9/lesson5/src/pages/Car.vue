b605fe4ad14b0fdb4c209b97ce96f18fda545d41
Anton Paraskiv <anton@paraskiv.net>
2019-11-16 11:25:13 +0300

    Added part1/block9/lesson5

---
+++ part1/block9/lesson5/src/pages/Car.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Car id {{ id }}</h1>
+</template>
+
+<script>
+    export default {
+        name: "Car",
+        data() {
+            return {
+                id: this.$route.params['id'],
+            }
+        },
+        watch: {
+            $route(toR, fromR) {
+                this.id = toR.params['id']
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
