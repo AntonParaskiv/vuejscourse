eaafad5dedd2b716b0447fd9a8a7dea0d8734b89
Anton Paraskiv <anton@paraskiv.net>
2019-11-17 16:47:36 +0300

    Added part1/block11/lesson6

---
+++ part1/block11/lesson6/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container text-center pt-5">
+    <h1> {{ title }}</h1>
+    <hr>
+    <app-counter></app-counter>
+    <app-second-counter></app-second-counter>
+    <hr>
+    <app-actions></app-actions>
+  </div>
+</template>
+
+<script>
+  import Counter from './Counter'
+  import SecondCounter from './SecondCounter'
+  import Actions from './Actions'
+
+    export default {
+        components: {
+            appCounter: Counter,
+            appSecondCounter: SecondCounter,
+            appActions: Actions,
+        },
+        computed: {
+            title() {
+              return this.$store.getters.title
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
