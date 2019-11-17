09e98e45e5a17740a26f9be258346e8f76b175df
Anton Paraskiv <anton@paraskiv.net>
2019-11-17 13:48:51 +0300

    Added part1/block10/lesson5

---
+++ part1/block10/lesson5/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueResourse from 'vue-resource'
+import App from './App.vue'
+
+Vue.use(VueResourse);
+Vue.http.options.root = 'http://localhost:3000/';
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+});
+
+
