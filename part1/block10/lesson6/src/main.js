aff69dd7ed5c265be491c8a6cea00ce82342c773
Anton Paraskiv <anton@paraskiv.net>
2019-11-17 13:57:36 +0300

    Added part1/block10/lesson6

---
+++ part1/block10/lesson6/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueResourse from 'vue-resource'
+import App from './App.vue'
+
+Vue.use(VueResourse);
+Vue.http.options.root = 'http://localhost:3000/';
+Vue.http.interceptors.push(request => {
+  request.headers.set('Auth', 'RAND TOKEN ' + Math.random());
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+});
+
+
