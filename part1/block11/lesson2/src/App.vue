6c99ebe90b66f84089117cc6d89b8532e4bba23a
Anton Paraskiv <anton@paraskiv.net>
2019-11-17 15:03:53 +0300

    Added part1/block11/lesson2

---
+++ part1/block11/lesson2/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container text-center pt-5">
+    <app-counter></app-counter>
+    <app-actions></app-actions>
+  </div>
+</template>
+
+<script>
+  import Counter from './Counter'
+  import Actions from './Actions'
+
+    export default {
+        components: {
+            appCounter: Counter,
+            appActions: Actions,
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
